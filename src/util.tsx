281961608026f673c94915f6dd0229596a7fd604
Captainlomik <nastiy248@mail.ru>
2023-04-16 13:25:30 +0300

    new data

--- src/util.tsx
+++ src/util.tsx
@@ -13,10 +13,30 @@ export default function chillHop() {
       audio: "https://mp3.chillhop.com/serve.php/?mp3=48087",
     },
     {
-      name: "Early Rise",
-      artist: "illiterate, Mr Slipz",
+      name: "About It",
+      artist: "Felty",
       cover:
-        "https://chillhop.com/wp-content/uploads/2022/09/8c2eab8525ace609b0c68b7564eb085d2244ee09-1024x1024.jpg",
+        "https://chillhop.com/wp-content/uploads/2023/04/a32d2ec47ba20c985c2b2425fd24d277a8f36d3e-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#bfccae", "#c2d2d1"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=48087",
+    },
+    {
+      name: "Cloud Zone",
+      artist: "Evil Needle, Misha",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2023/02/71374d07888a8b1b3fbef53b82d283f53209cc1a-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#bfccae", "#c2d2d1"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=48087",
+    },
+    {
+      name: "Oh Let's Ride",
+      artist: "Teddy Roxpin",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/09/8504dc266e0c0d7c3c9e3581b1c34426648a4027-1024x1024.jpg",
       id: uuidv4(),
       active: false,
       color: ["#bfccae", "#c2d2d1"],
